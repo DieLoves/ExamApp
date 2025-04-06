9b59fcc6246812f3c9894714f59fc37d01d1d04f
DieLoves <insar667@gmail.com>
2025-04-06 15:53:17 +0500

    Next config update

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,8 @@ console.log(APP_VERSION);
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	output: 'export', // Outputs a Single-Page Application (SPA).
-	distDir: APP_VERSION == 'universal' ? './dist' : `./dist/${APP_VERSION}`, // Changes the build output directory to `./dist/`.
+	distDir: APP_VERSION == 'universal' ? undefined : `./dist/${APP_VERSION}`, // Changes the build output directory to `./dist/`.
+	cleanDistDir: true,
 };
 
 export default nextConfig;
